00667cd3816f88c8bd856ac5fe7a2a8026cd7267
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-10-07 10:57:43 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -23,16 +23,14 @@ cd opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D INSTALL_C_EXAMPLES=OFF \
-      -D WITH_TBB=ON \
-      -D WITH_V4L=ON \
-      -D WITH_QT=OFF \
-      -D BUILD_PERF_TESTS=OFF \
-      -D BUILD_TESTS=OFF 
-      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
       -D BUILD_EXAMPLES=OFF \
-      -D WITH_CUDA=OFF ..
+      -D BUILD_opencv_apps=OFF \
+       -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D BUILD_DOCS=OFF \
+      -D BUILD_PERF_TESTS=OFF \
+      -D BUILD_TESTS=OFF \
+      -D WITH_CUDA=OFF \
+      -D CMAKE_INSTALL_PREFIX=/usr/local ..
       
 make 
 sudo make install
