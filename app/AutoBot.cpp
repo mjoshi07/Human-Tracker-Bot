39959fc7d5cbb64480b6f2cee3600ae46767ff12
Naitri <naitrir21@gmail.com>
2021-10-17 13:43:09 -0400

    1.23 implemented run and init params method of AutoBot class

--- app/AutoBot.cpp
+++ app/AutoBot.cpp
@@ -24,7 +24,7 @@
 
 /**
  * @file AutoBot.cpp
- * @author Mayank Joshi (driver) and Naitri Rajyaguru (navigator)
+ * @author Phase:1 Naitri Rajyaguru(driver) and Mayank Joshi(navigator)
  * @brief AutoBot class definition for Acme Robotics - Human Tracker
  * @version 0.1
  * @date 2021-10-05
@@ -33,16 +33,31 @@
  * 
  */
 
-#include <AutoBot.h>
 
 
-acme::AutoBot::AutoBot() {}
+#include <AutoBot.hpp>
 
-acme::AutoBot::AutoBot(bool is_calibrated, double calib_factor) {}
+acme::AutoBot::AutoBot(const int camera_id, double calib_factor) {
+    /// initializes default parameters
+    InitParams(camera_id, calib_factor);
+}
 
 acme::AutoBot::~AutoBot() {}
 
-void acme::AutoBot::Run(const Mode mode) {}
+void acme::AutoBot::Run(const Mode mode) {
+    /// Based on the mode selected, run the specific module
+    switch ( static_cast<int>(mode) ) {
+        case 1:
+            CollectTrainData();
+            break;
+        case 2:
+            DetectTestData();
+            break;
+        default:
+            RunRealTime();
+            break;
+    }
+}
 
 void acme::AutoBot::SetRobotPose(const Pose &robo_pose) {}
 
@@ -55,3 +70,62 @@ std::vector<acme::Pose>  acme::AutoBot::RunRealTime() {
 }
 
 void acme::AutoBot::Calibrate() {}
+
+
+void acme::AutoBot::InitParams(int camera_id, double calib_factor) {
+    /// set camera id
+    camera_id_ = camera_id;
+
+    /// set calib_factor default value
+    calib_factor_ = calib_factor;
+
+    objects_in_frame_.clear();
+
+    /// set focal length in meters default value
+    focal_length_ = 0.036;
+
+    /// set human height, in meters default value
+    avg_human_height_ = 1.7;
+
+
+    /// open camera stream using the video capture object
+    cap_.open(camera_id_);
+
+    /// check if camera is open
+    if ( cap_.isOpened() ) {
+        /// set srouce frame width
+        frame_width_ = cap_.get(cv::CAP_PROP_FRAME_WIDTH);
+
+        /// set srouce frame height
+        frame_height_ = cap_.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+        // set srouce frame rate
+        frame_rate_ = cap_.get(cv::CAP_PROP_FPS);
+
+        /// if height or width is 0, throw error
+        if (frame_height_ == 0 || frame_width_ == 0) {
+            // Display error msg and EXIT
+            std::cout << "Frame is empty" << std::endl;
+            return;
+        }
+    } else {
+            // Display error msg and EXIT
+            std::cout << "Cannot open camera" << std::endl;
+            return;
+    }
+
+    /// set processing size default value
+    p_size_ = cv::Size(640, 480);
+
+    /// set confidence threshold for human_tracker default value
+    double conf_threshold = 0.6;
+
+    /// initialize HumanTracker Object
+    human_tracker_ = std::make_unique<acme::HumanTracker>(conf_threshold);
+
+    /// set show window default value
+    show_window_ = true;
+
+    /// set test counter default value
+    test_counter_ = -1;
+}
