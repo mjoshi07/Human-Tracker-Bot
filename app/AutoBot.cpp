1e97b0e5a2e679b2dcad91b52082409078e6b22d
Naitri <naitrir21@gmail.com>
2021-10-25 03:19:40 -0400

    2.9 Implemented AutoBot Class

--- app/AutoBot.cpp
+++ app/AutoBot.cpp
@@ -24,7 +24,7 @@
 
 /**
  * @file AutoBot.cpp
- * @author Phase:1 Naitri Rajyaguru(driver) and Mayank Joshi(navigator)
+ * @author Phase:2 Naitri Rajyaguru(driver) and Mayank Joshi(navigator)
  * @brief AutoBot class definition for Acme Robotics - Human Tracker
  * @version 0.1
  * @date 2021-10-05
@@ -32,7 +32,7 @@
  * @copyright Copyright (c) 2021
  * 
  */
-
+#include <eigen3/Eigen/Dense>
 
 
 #include <AutoBot.hpp>
@@ -94,14 +94,81 @@ void acme::AutoBot::SetTestCounter(const int counter) {
 }
 
 std::vector<acme::Pose> acme::AutoBot::GetObjects() {
-    return std::vector<acme::Pose>();
+    return objects_in_frame_;
 }
 
 
-void acme::AutoBot::CollectTrainData() {}
+void acme::AutoBot::CollectTrainData() {
+    std::string address;
+    /// Number of frames to be captured
+    int frame_count = 0;
+    /// Get address where images are to be saved
+    /// Uncommnent the lines below while running in training mode
+    /// std::cout << "Enter the full path to save frames" << std::endl;
+    /// std::getline(std::cin, address);
 
-void acme::AutoBot::DetectTestData() {}
+    /// comment the line below when running in training mode
+    address = "..//data//train//";
 
+    /// Uncommnent the lines below while running in training mode
+    /// Get number of frames to be captured
+    /// std::cout << "Enter frame count" << std::endl;
+    // std::getline(std::cin,frame_count);
+    /// std::cin >> frame_count;
+
+    /// comment the line below when running in training mode
+    frame_count = 2;
+    cv::VideoCapture cap(camera_id_);
+
+  if (!cap.isOpened()) {
+    std::cerr << "ERROR: Could not open video " << std::endl;
+    return;
+  } else {
+        for (int counter = 0; counter < frame_count; counter++) {
+    cv::Mat frame;
+    cap >> frame;
+    std::string file_name = address+"//"+std::to_string(counter) + ".jpg";
+    cv::imwrite(file_name, frame);
+  }
+}
+
+  return;
+}
+
+
+void acme::AutoBot::DetectTestData() {
+    cap_.release();
+    cv::Mat img1 = cv::imread("..//data//test//img1.png");
+    cv::Mat img2 = cv::imread("..//data//test//img2.png");
+    cv::Mat img3 = cv::imread("..//data//test//img3.png");
+    cv::Mat img4 = cv::imread("..//data//test//img4.png");
+
+    /// ground truth for img 2
+    cv::Rect ground_truth_2(330, 107, 160, 294);
+
+    /// ground truth for img 3
+    cv::Rect ground_truth_3(298, 126, 98, 228);
+
+    /// ground truth for img 1
+    cv::Rect ground_truth_1(300, 21, 197, 310);
+
+    /// ground truth for img 4
+    cv::Rect ground_truth_4(297, 84, 147, 265);
+
+    std::vector <cv::Rect> ground_truth = { ground_truth_1, ground_truth_2,
+                            ground_truth_3, ground_truth_4};
+    std::vector <cv::Mat> imgs = {img1, img2, img3, img4};
+    for (int i = 0; i < static_cast<int>(imgs.size()); i++) {
+        p_frame_ = acme::Utils::ResizeImage(imgs[i], p_size_);
+        cv::Rect box2;
+        /// get bboxes of objects tracked by HumanTracker class in frame
+        box2 = human_tracker_->TrackHumans(p_frame_)[0];
+
+        double iou = acme::Utils::CalculateIOU(ground_truth[i], box2);
+        std::cout << "IoU for image " + std::to_string(i+1) + ": " +
+        std::to_string(iou) << std::endl;
+    }
+}
 
 
 void acme::AutoBot::RunRealTime() {
@@ -144,7 +211,7 @@ void acme::AutoBot::RunRealTime() {
             human_tracks = human_tracker_->TrackHumans(p_frame_);
 
             /// draw bboxes on the frame
-            p_frame_ = Utils::DrawBbox(p_frame_, human_tracks);
+            //  p_frame_ = Utils::DrawBbox(p_frame_, human_tracks);
 
             /// convert the bboxes from img plane to robot ref frame
             ToRobotFrame(human_tracks);
@@ -178,7 +245,42 @@ void acme::AutoBot::RunRealTime() {
     }
 }
 
-void acme::AutoBot::Calibrate() {}
+void acme::AutoBot::Calibrate() {
+    /// calculate calib factor and focal length here
+    cv::Mat image = cv::imread("..//data//calibrate.jpg", 0);
+    cv::Mat thr;
+    /// original distance between object and camera
+    double original_distance = 20;
+    /// original width of the object
+    double original_width = 10;
+    /// thresholding the image
+    cv::threshold(image, thr, 150, 255, cv::THRESH_BINARY);
+    /// variable tostore contours
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    /// search for the contour
+    cv::findContours(thr, contours, hierarchy,
+                    cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    double maxArea = 0;
+    int maxAreaContourId = -1;
+    /// get ID for contour of maximum area
+    for (int j = 0; j < static_cast<int>(contours.size()); j++) {
+        double newArea = cv::contourArea(contours.at(j));
+        if (newArea > maxArea) {
+            maxArea = newArea;
+            maxAreaContourId = j;
+        }
+    }
+    /// make bounding box
+    cv::Rect box = cv::boundingRect(contours[maxAreaContourId]);
+    /// calculate focal length
+    double focalLength = (box.width * original_distance) / original_width;
+    std::cout << focalLength;
+    /// set focal length
+    SetFocalLength(focalLength);
+    calib_factor_ = focalLength * avg_human_height_;
+}
+
 
 
 void acme::AutoBot::InitParams(int camera_id, double calib_factor) {
@@ -241,27 +343,56 @@ void acme::AutoBot::InitParams(int camera_id, double calib_factor) {
             std::cout << "Cannot open camera" << std::endl;
             return;
     }
+
+    // initialize robot pose
+    robot_pose_ = acme::Pose(7, 5, 8);
+
+    // initialize camera pose
+    camera_pose_ = acme::Pose(2, 2, 3);
 }
 
 void acme::AutoBot::ToRobotFrame(const std::vector<cv::Rect> &tracks) {
     objects_in_frame_.clear();
+    Eigen::Matrix4d transform_mat;
+    Eigen::Vector4d obj_in_cam;
+    Eigen::Vector4d obj_in_robot;
+    /// transformation matrix considering
+    transform_mat << 1, 0, 0, robot_pose_.x - camera_pose_.x,
+                     0, 1, 0, robot_pose_.y - camera_pose_.y,
+                     0, 0, 1, robot_pose_.z - camera_pose_.z,
+                     0, 0, 0, 1;
+
     if ( tracks.size() ) {
         /// create a variable to store Pose of the detected object
         acme::Pose obj_pose;
+        int bbox_id = 0;
         for ( cv::Rect box : tracks ) {
+            bbox_id++;
+            double factor = avg_human_height_ / box.height;
             /// convert from img plane to Pose(x, y, z) using calib_factor
             obj_pose = acme::Utils::PixelsToPose(box, calib_factor_);
+            obj_pose.y = obj_pose.y * factor;
+            obj_pose.z = obj_pose.z * factor;
 
             // from world coordinates to camera coordinates using homogeneous
             // CHECK UTILS GET TRANSFORMED POSE METHOD
-
-            /// convert from camera Pose to Pose
-            obj_pose.x -= robot_pose_.x;
-            obj_pose.y -= robot_pose_.y;
-            obj_pose.z -= robot_pose_.z;
+            obj_in_cam << obj_pose.x , obj_pose.y, obj_pose.z , 1;
+            /// convert from camera Pose to Robot Pose
+            obj_in_robot = transform_mat * obj_in_cam;
 
             /// store detected obj Pose in a vector
-            objects_in_frame_.push_back(obj_pose);
+            objects_in_frame_.push_back(acme::Pose(obj_in_robot[0],
+                obj_in_robot[1], obj_in_robot[2]));
+
+            std::string label = std::to_string(bbox_id) + " (" +
+                            std::to_string(static_cast<int>(obj_in_robot[0]))
+                                + ","+
+                            std::to_string(static_cast<int>(obj_in_robot[1]))
+                                + ","+
+                            std::to_string(static_cast<int>(obj_in_robot[2]))
+                                + ")";
+
+            p_frame_ = acme::Utils::DrawBbox(p_frame_, box, label);
         }
     }
 }
